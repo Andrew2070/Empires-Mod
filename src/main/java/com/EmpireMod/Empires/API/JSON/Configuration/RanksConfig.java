36254ca45a4954ac49234d6bdcbbe15402f09066
Andrew <mohammadshah44@gmail.com>
2018-05-05 16:55:37 +0500

    Fixed Rank Glitch

--- src/main/java/com/EmpireMod/Empires/API/JSON/Configuration/RanksConfig.java
+++ src/main/java/com/EmpireMod/Empires/API/JSON/Configuration/RanksConfig.java
@@ -1,5 +1,6 @@
 package com.EmpireMod.Empires.API.JSON.Configuration;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,62 +10,60 @@ import com.google.gson.GsonBuilder;
 
 public class RanksConfig extends JsonConfig<Rank, Rank.Container> {
 
-	public RanksConfig(String path) {
-		super(path, "DefaultEmpireRanks");
-		this.gsonType = Rank.Container.class;
-		this.gson = new GsonBuilder().registerTypeAdapter(Rank.class, new Rank.Serializer()).setPrettyPrinting()
-				.create();
-	}
+    public RanksConfig(String path) {
+        super(path, "DefaultEmpireRanks");
+        this.gsonType = Rank.Container.class;
+        this.gson = new GsonBuilder().registerTypeAdapter(Rank.class, new Rank.Serializer()).setPrettyPrinting().create();
+    }
 
-	@Override
-	protected Rank.Container newList() {
-		return new Rank.Container();
-	}
+    @Override
+    protected Rank.Container newList() {
+        return new Rank.Container();
+    }
 
-	@Override
-	public void create(Rank.Container items) {
-		Rank.initDefaultRanks();
-		items.addAll(Rank.defaultRanks);
-		super.create(items);
-	}
+    @Override
+    public void create(Rank.Container items) {
+        Rank.initDefaultRanks();
+        items.addAll(Rank.defaultRanks);
+        super.create(items);
+    }
 
-	@Override
-	public Rank.Container read() {
-		Rank.Container ranks = super.read();
+    @Override
+    public Rank.Container read() {
+        Rank.Container ranks = super.read();
 
-		Rank.defaultRanks.clear();
-		Rank.defaultRanks.addAll(ranks);
+        Rank.defaultRanks.clear();
+        Rank.defaultRanks.addAll(ranks);
 
-		return ranks;
-	}
+        return ranks;
+    }
 
-	@Override
-	public boolean validate(Rank.Container items) {
-		boolean isValid = true;
-		for (Rank.Type type : Rank.Type.values()) {
-			if (type.unique) {
-				List<Rank> rankOfType = new ArrayList<Rank>();
-				for (Rank rank : items) {
-					if (rank.getType() == type) {
-						rankOfType.add(rank);
-					}
-				}
+    @Override
+    public boolean validate(Rank.Container items) {
+        boolean isValid = true;
+        for(Rank.Type type : Rank.Type.values()) {
+            if(type.unique) {
+                List<Rank> rankOfType = new ArrayList<Rank>();
+                for(Rank rank : items) {
+                    if(rank.getType() == type) {
+                        rankOfType.add(rank);
+                    }
+                }
 
-				if (rankOfType.size() == 0) {
-					isValid = false;
-					Empires.instance.LOG.error("Unique type of Rank was not found in " + name);
-					items.add(Rank.defaultRanks.get(type));
-				} else if (rankOfType.size() > 1) {
-					isValid = false;
-					Empires.instance.LOG.error("Unique type of Rank was found multiple times in " + name
-							+ ". Setting all aside from the first to type regular.");
-					for (int i = 1; i < rankOfType.size(); i++) {
-						rankOfType.get(i).setType(Rank.Type.REGULAR);
-					}
-				}
-			}
-		}
+                if(rankOfType.size() == 0) {
+                    isValid = false;
+                    Empires.instance.LOG.error("Unique type of Rank was not found in " + name);
+                    items.add(Rank.defaultRanks.get(type));
+                } else if(rankOfType.size() > 1) {
+                    isValid = false;
+                    Empires.instance.LOG.error("Unique type of Rank was found multiple times in " + name + ". Setting all aside from the first to type regular.");
+                    for(int i = 1; i < rankOfType.size(); i++) {
+                        rankOfType.get(i).setType(Rank.Type.OFFICER);
+                    }
+                }
+            }
+        }
 
-		return isValid;
-	}
+        return isValid;
+    }
 }
