b9887cda677eede42e29800d9dd5a23efecd4b92
Andrew <mohammadshah44@gmail.com>
2018-07-05 23:36:15 +0500

    Added Creative Check To PvPUtils

--- src/main/java/EmpiresMod/Utilities/PvPUtils.java
+++ src/main/java/EmpiresMod/Utilities/PvPUtils.java
@@ -1,5 +1,7 @@
 package EmpiresMod.Utilities;
 
+import java.util.List;
+
 import EmpiresMod.Empires;
 import EmpiresMod.API.Chat.Component.ChatManager;
 import EmpiresMod.API.Commands.Command.CommandsEMP;
@@ -10,9 +12,35 @@ import EmpiresMod.entities.Empire.Empire;
 import EmpiresMod.exceptions.Command.CommandException;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 
 public class PvPUtils {
-
+	
+	
+	public static Boolean enemyNearby(EntityPlayer localPlayer) {
+		List<EntityPlayerMP> allPlayers = MinecraftServer.getServer().getConfigurationManager().playerEntityList;
+	     for(int i=0; i < allPlayers.size(); i++) {
+			EntityPlayer player = allPlayers.get(i);
+			
+			double x1 = localPlayer.posX;
+			double y1 = localPlayer.posY;
+			double z1 = localPlayer.posZ;
+			
+			double x2 = player.posX;
+			double y2 = player.posY;
+			double z2 = player.posZ;
+			
+			double distance = Math.sqrt(Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2));
+			
+			if (distance < 64) {
+				return true;
+			}
+	     }
+		return false;
+		
+	}
+	
 	public static void deathProcess(Entity attacker, Entity defender, float damage) {
 		if (defender instanceof EntityPlayer) {
 			EntityPlayer defendingPlayer = (EntityPlayer) defender;
@@ -21,9 +49,10 @@ public class PvPUtils {
 			if (attacker instanceof EntityPlayer) { //PVP
 				EntityPlayer attackingPlayer = (EntityPlayer) attacker;
 				Citizen attackerCitizen = EmpiresUniverse.instance.getOrMakeCitizen(attacker);
-				if (damage >= defendingPlayer.getHealth()) {
-				if (defendingCitizen.getPower() < Config.instance.minPower.get()) {
-					defendingCitizen.setPower(Config.instance.minPower.get());
+				if (defendingPlayer.capabilities.isCreativeMode = false) {
+					if (damage >= defendingPlayer.getHealth()) {
+						if (defendingCitizen.getPower() < Config.instance.minPower.get()) {
+							defendingCitizen.setPower(Config.instance.minPower.get());
 					//Just a check + fix to make sure a player's power is not too negative. 
 				}
 				attackerCitizen.addPower(Config.instance.pvpPowerTransfer.get());
@@ -45,6 +74,8 @@ public class PvPUtils {
 			  }
 			}
 			}
+			}
+		if (defendingPlayer.capabilities.isCreativeMode = false) {
 			if (damage >= defendingPlayer.getHealth()) {
 				if (defendingCitizen.getPower() < Config.instance.minPower.get()) {
 					defendingCitizen.setPower(Config.instance.minPower.get());
@@ -62,6 +93,7 @@ public class PvPUtils {
 				}
 				}
 			}
+		}
 		}
 		
 	}
