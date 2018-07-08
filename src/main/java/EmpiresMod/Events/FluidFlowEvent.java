b319c3c7884062f53f9a6cf331581df2f4d24155
Andrew <mohammadshah44@gmail.com>
2018-07-08 18:19:13 +0500

    1.8 Update

---
+++ src/main/java/EmpiresMod/Events/FluidFlowEvent.java
@@ -0,0 +1,13 @@
+package EmpiresMod.Events;
+
+import net.minecraft.block.Block;
+
+public class FluidFlowEvent {
+	 public int x, y, z;
+	    public Block block;
+
+	    public FluidFlowEvent(int x, int y, int z, Block block) {
+
+	    }
+
+}
